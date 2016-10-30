c2d05dcf8e484020ccf153f2eefdbf52b5eeb950
george99g <georgi_99g@abv.bg>
2016-10-30 19:52:17 +0200

    Update documentation.

---
+++ search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghijklmnopqrstuvwxyz~",
+  1: "ceikmnoqstw",
+  2: "q",
+  3: "abcdefghijklmnopqrstuvwxyz~",
+  4: "abcdefghiklmnoprstuvw",
+  5: "cg",
+  6: "acdefghilmnopqrstu",
+  7: "d"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "functions",
+  4: "variables",
+  5: "enums",
+  6: "properties",
+  7: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Functions",
+  4: "Variables",
+  5: "Enumerations",
+  6: "Properties",
+  7: "Pages"
+};
+

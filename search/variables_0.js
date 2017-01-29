6f94f008accbf2ac7e8077f54eacd1a4b0d4957b
george99g <georgi_99g@abv.bg>
2017-01-29 15:03:50 +0200

    Update documentation.

--- search/variables_0.js
+++ search/variables_0.js
@@ -3,6 +3,8 @@ var searchData=
   ['alignment',['Alignment',['https://doc.qt.io/qt-5/qt.html#AlignmentFlag-enum',1,'Qt']]],
   ['alternatenameentrytype',['AlternateNameEntryType',['https://doc.qt.io/qt-5/qssl-obsolete.html#AlternateNameEntryType-typedef',1,'QSsl']]],
   ['apibase',['apiBase',['../namespaceQDiscordUtilities.html#ab2c7f382b08078007e68357f6bbf034f',1,'QDiscordUtilities::EndPoints']]],
+  ['apiversion',['apiVersion',['../namespaceQDiscordUtilities.html#a365c20207f54225c2f3ed0229e33a1aa',1,'QDiscordUtilities']]],
   ['applicationstates',['ApplicationStates',['https://doc.qt.io/qt-5/qt.html#ApplicationState-enum',1,'Qt']]],
-  ['attributesmap',['AttributesMap',['https://doc.qt.io/qt-5/qnetworkcachemetadata.html#AttributesMap-typedef',1,'QNetworkCacheMetaData']]]
+  ['attributesmap',['AttributesMap',['https://doc.qt.io/qt-5/qnetworkcachemetadata.html#AttributesMap-typedef',1,'QNetworkCacheMetaData']]],
+  ['auth',['auth',['../namespaceQDiscordUtilities.html#a43f6b61ead601e701ac29bb2f502f45e',1,'QDiscordUtilities::EndPoints']]]
 ];
